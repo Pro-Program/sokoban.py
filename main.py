a58070fafea2aaab05bdfd5a667c85ce3c5d4a27
Gordon Li <gordonlin2011@gmail.com>
2020-11-28 12:49:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ screen = pygame.display.set_mode((width,height))
 done = False
 while not done:
   for event in pygame.event.get():
-    
+    if event.type = pygame.QUIT:
+    	done=True
 pygame.quit()
