ffe16b91f25378e4ad1ad73d8053559b233a1973
Gordon Li <gordonlin2011@gmail.com>
2020-11-28 17:56:05 +0000

    sokoban.py updated

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ pygame.init()
 width=400
 height=400
 screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption('Sokoban')
 done = False
 while not done:
   for event in pygame.event.get():
